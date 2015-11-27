8bbb2b35d29e6cc1a7c2598bc67805dd30794af1
Raghu <rp4raghuprasad@gmail.com>
2015-11-28 00:31:22 +0530

    moved to a single file

--- awesome_idoms.py
+++ awesome_idoms.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+
+# List comprehension to get the sum of squares of all numbers
+l = [1, 2, 3, 4, 5]
+print sum([x*x for x in l])
+
 # The below snippet retuns max number of times repeated number in a given list.
 l = [1, 2, 3, 3, 2, 1, 2, 5]
 print max(set(l), key=l.count)
--- list_comp.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-# List comprehension to get the sum of squares of all numbers
-l = [1, 2, 3, 4, 5]
-print sum([x*x for x in l])
