ef3cdcbf9f33a2be59ffaa97a7a74f1790761726
rp4raghup <rp4raghuprasad@gmail.com>
2015-12-28 21:37:01 +0530

    Update awesome_idoms.py

--- awesome_idoms.py
+++ awesome_idoms.py
@@ -4,6 +4,11 @@
 l = [1, 2, 3, 4, 5]
 print sum([x*x for x in l])
 
-# The below snippet retuns max number of times repeated number in a given list.
+# The below snippet retuns max number of times repeated number in a given list
 l = [1, 2, 3, 3, 2, 1, 2, 5]
 print max(set(l), key=l.count)
+
+# Intersection of two lists l1, l2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+l = list(set(list1).intersection(list2))
