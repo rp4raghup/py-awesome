a8ac50fe9ca7b5e7f2f493ba3546be95e5b8b997
Raghu Prasad R <rp4raghuprasad@gmail.com>
2017-12-10 22:00:22 +0530

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,6 @@
 # List compression basics
 
+################################################
 # Ex1: Square each element of a list.
 source_list = [1, 2, 3, 4, 5]
 
@@ -13,3 +14,32 @@ print dest_list
 
 # LC Way
 dest_list = [ele*ele for ele in source_list]
+
+###############################################
+# Ex2: Filter all odd numbers from list.
+source_list = [1, 2, 3, 4, 5, 6]
+
+# Traditional way
+dest_list = []
+for ele in source_list:
+  if ele % 2 != 0:
+    dest_list.append(ele)
+
+print dest_list
+
+# LC Way
+dest_list = [ele for ele in source_list if ele % 2 != 0]
+
+#################################################
+# Ex3: Removing vowels from sentence.
+source_str = "this is awesome" # output should be: "ths s wsm"
+
+# Traditional way
+dest_str = []
+for ele in source_str:
+  if ele not in ['a', 'e', 'i', 'o', 'u']:
+    dest_str.append(ele)
+
+answer = "".join(dest_str)
+
+# LC Way??
